8dd3673db326d1e5a765aec8576d389e9139cf51
Diego Tori <diego.tori@greenfence.com>
2024-12-20 19:44:57 -0500

    Fixed dart formatting issues.

--- wakelock_plus_platform_interface/lib/messages.g.dart
+++ wakelock_plus_platform_interface/lib/messages.g.dart
@@ -15,7 +15,8 @@ PlatformException _createConnectionError(String channelName) {
   );
 }
 
-List<Object?> wrapResponse({Object? result, PlatformException? error, bool empty = false}) {
+List<Object?> wrapResponse(
+    {Object? result, PlatformException? error, bool empty = false}) {
   if (empty) {
     return <Object?>[];
   }
@@ -69,7 +70,6 @@ class IsEnabledMessage {
   }
 }
 
-
 class _PigeonCodec extends StandardMessageCodec {
   const _PigeonCodec();
   @override
@@ -77,10 +77,10 @@ class _PigeonCodec extends StandardMessageCodec {
     if (value is int) {
       buffer.putUint8(4);
       buffer.putInt64(value);
-    }    else if (value is ToggleMessage) {
+    } else if (value is ToggleMessage) {
       buffer.putUint8(129);
       writeValue(buffer, value.encode());
-    }    else if (value is IsEnabledMessage) {
+    } else if (value is IsEnabledMessage) {
       buffer.putUint8(130);
       writeValue(buffer, value.encode());
     } else {
@@ -91,9 +91,9 @@ class _PigeonCodec extends StandardMessageCodec {
   @override
   Object? readValueOfType(int type, ReadBuffer buffer) {
     switch (type) {
-      case 129: 
+      case 129:
         return ToggleMessage.decode(readValue(buffer)!);
-      case 130: 
+      case 130:
         return IsEnabledMessage.decode(readValue(buffer)!);
       default:
         return super.readValueOfType(type, buffer);
@@ -105,9 +105,11 @@ class WakelockPlusApi {
   /// Constructor for [WakelockPlusApi].  The [binaryMessenger] named argument is
   /// available for dependency injection.  If it is left null, the default
   /// BinaryMessenger will be used which routes to the host platform.
-  WakelockPlusApi({BinaryMessenger? binaryMessenger, String messageChannelSuffix = ''})
+  WakelockPlusApi(
+      {BinaryMessenger? binaryMessenger, String messageChannelSuffix = ''})
       : pigeonVar_binaryMessenger = binaryMessenger,
-        pigeonVar_messageChannelSuffix = messageChannelSuffix.isNotEmpty ? '.$messageChannelSuffix' : '';
+        pigeonVar_messageChannelSuffix =
+            messageChannelSuffix.isNotEmpty ? '.$messageChannelSuffix' : '';
   final BinaryMessenger? pigeonVar_binaryMessenger;
 
   static const MessageCodec<Object?> pigeonChannelCodec = _PigeonCodec();
@@ -115,8 +117,10 @@ class WakelockPlusApi {
   final String pigeonVar_messageChannelSuffix;
 
   Future<void> toggle(ToggleMessage msg) async {
-    final String pigeonVar_channelName = 'dev.flutter.pigeon.wakelock_plus_platform_interface.WakelockPlusApi.toggle$pigeonVar_messageChannelSuffix';
-    final BasicMessageChannel<Object?> pigeonVar_channel = BasicMessageChannel<Object?>(
+    final String pigeonVar_channelName =
+        'dev.flutter.pigeon.wakelock_plus_platform_interface.WakelockPlusApi.toggle$pigeonVar_messageChannelSuffix';
+    final BasicMessageChannel<Object?> pigeonVar_channel =
+        BasicMessageChannel<Object?>(
       pigeonVar_channelName,
       pigeonChannelCodec,
       binaryMessenger: pigeonVar_binaryMessenger,
@@ -137,8 +141,10 @@ class WakelockPlusApi {
   }
 
   Future<IsEnabledMessage> isEnabled() async {
-    final String pigeonVar_channelName = 'dev.flutter.pigeon.wakelock_plus_platform_interface.WakelockPlusApi.isEnabled$pigeonVar_messageChannelSuffix';
-    final BasicMessageChannel<Object?> pigeonVar_channel = BasicMessageChannel<Object?>(
+    final String pigeonVar_channelName =
+        'dev.flutter.pigeon.wakelock_plus_platform_interface.WakelockPlusApi.isEnabled$pigeonVar_messageChannelSuffix';
+    final BasicMessageChannel<Object?> pigeonVar_channel =
+        BasicMessageChannel<Object?>(
       pigeonVar_channelName,
       pigeonChannelCodec,
       binaryMessenger: pigeonVar_binaryMessenger,
