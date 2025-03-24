16ec14bc0f251ad6e1257060ca455c5fdbc9a7c4
swi-oberhauser <august.oberhauser@swissinfo.ch>
2025-03-24 15:59:03 +0100

    Add Swift Package Manager Support (closes #80)

--- wakelock_plus/pigeons/messages.dart
+++ wakelock_plus/pigeons/messages.dart
@@ -13,10 +13,11 @@ class IsEnabledMessage {
 @ConfigurePigeon(PigeonOptions(
   dartOut: '../wakelock_plus_platform_interface/lib/messages.g.dart',
   dartTestOut: '../wakelock_plus_platform_interface/test/messages.g.dart',
-  objcHeaderOut: 'ios/Classes/messages.g.h',
-  objcSourceOut: 'ios/Classes/messages.g.m',
+  objcHeaderOut: 'ios/wakelock_plus/Sources/wakelock_plus/include/wakelock_plus/messages.g.h',
+  objcSourceOut: 'ios/wakelock_plus/Sources/wakelock_plus/messages.g.m',
   objcOptions: ObjcOptions(
     prefix: 'WAKELOCKPLUS',
+    headerIncludePath: './include/wakelock_plus/messages.g.h',
   ),
   kotlinOptions: KotlinOptions(errorClassName: "WakelockPlusFlutterError"),
   kotlinOut:
